7e97761fdb4517f46c6c7551b8fa0e2cd0dbcd93
Limbasan David <lazardavid50@gmail.com>
2024-05-09 23:57:08 +0300

    am pus profilul

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -143,7 +143,7 @@ mysqli_close($conn);
         @if (Route::has('login'))
         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
             @auth
-                <a href="{{ url('/dashboard') }}" class="btn btn-sm btn-outline-dark mx-2">profile</a>
+                <a href="{{ url('/profil') }}" class="btn btn-sm btn-outline-dark mx-2">profile</a>
             @else
                 <a href="{{ route('login') }}" class="btn btn-sm btn-outline-dark mx-2">Log in</a>
 
